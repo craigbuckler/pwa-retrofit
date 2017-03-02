485d1950f3783d4dd69c1d8193c2433cb90aecbf
Craig Buckler <craig@optimalworks.net>
2017-03-02 12:34:31 +0000

    Demonstration web pages and server

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# pwa-retrofit
-Retro-fit Progressive Web App technologies to a website.
+# PWA retrofit
+
+Progressive Web Apps are *progressive enhancements*. Supported browers can offer installation and offline functionality but unsupported browsers still get an online experience.
+
+The technology is typically used for web apps but can be applied to any website as demonstrated here.
+
+
+## Usage
+Ensure [Node.js](https://nodejs.org/) installed then start the web server with:
+
+    node ./server.js [port]
+
+`[port]` is optional and defaults to 8888.
+
+Load the demonstration home page at [http://localhost:8888/](http://localhost:8888/) (or which ever port you specified). Recommendations:
+
+1. Open the site in Chrome's incognito window.
+1. Open the Developer Tools and View the **Application** tab.
---
+++ server.js
@@ -0,0 +1,89 @@
+#!/usr/bin/env node
+
+/*
+server.js: launches a static file web server from the current folder
+
+make executable with `chmod +x ./server.js`
+run with `./server.js [port]`
+where `[port]` is an optional HTTP port (8888 by default)
+*/
+(function() {
+
+'use strict';
+
+const
+  http  = require('http'),
+  url   = require('url'),
+  path  = require('path'),
+  fs    = require('fs'),
+  port  = parseInt(process.argv[2] || 8888, 10),
+  mime  = {
+    '.html' : ['text/html', 86400],
+    '.htm'  : ['text/html', 86400],
+    '.css'  : ['text/css', 86400],
+    '.js'   : ['application/javascript', 86400],
+    '.json' : ['application/json', 1800],
+    '.jpg'  : ['image/jpeg', 0],
+    '.jpeg' : ['image/jpeg', 0],
+    '.png'  : ['image/png', 0],
+    '.gif'  : ['image/gif', 0],
+    '.ico'  : ['image/x-icon', 0],
+    '.svg'  : ['image/svg+xml', 0],
+    '.txt'  : ['text/plain', 86400],
+    'err'   : ['text/plain', 30]
+  };
+
+// new server
+http.createServer(function(req, res) {
+
+  let
+    uri = url.parse(req.url).pathname,
+    filename = path.join(process.cwd(), uri);
+
+  // file available?
+  fs.access(filename, fs.constants.R_OK, (err) => {
+
+    // not found
+    if (err) {
+      serve(404, '404 Not Found\n');
+      return;
+    }
+
+    // index.html default
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    // read file
+    fs.readFile(filename, (err, file) => {
+
+      if (err) {
+        // error reading
+        serve(500, err + '\n');
+      }
+      else {
+        // return file
+        serve(200, file, path.extname(filename));
+      }
+
+    });
+  });
+
+  // serve content
+  function serve(code, content, type) {
+
+    let head = mime[type] || mime['err'];
+
+    res.writeHead(code, {
+      'Content-Type'    : head[0],
+      'Cache-Control'   : 'max-age=' + (head[1] || 2419200),
+      'Content-Length'  : Buffer.byteLength(content)
+    });
+    res.write(content);
+    res.end();
+
+  }
+
+}).listen(port);
+
+console.log('Server running at http://localhost:' + port);
+
+}());
